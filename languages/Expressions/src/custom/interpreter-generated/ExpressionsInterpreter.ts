89b3ac3a30926313fece2598ac30e7ba84f98143
Jos Warmer <jos.warmer@openmodeling.nl>
2025-01-23 11:27:16 +0100

    Add files

---
+++ languages/Expressions/src/custom/interpreter-generated/ExpressionsInterpreter.ts
@@ -0,0 +1,16 @@
+// Generated my Freon once, will NEVER be overwritten.
+import { IMainInterpreter } from '@freon4dsl/core';
+import { ExpressionsInterpreterBase } from './gen/ExpressionsInterpreterBase.js';
+
+let main: IMainInterpreter;
+
+/**
+ * The class containing all interpreter functions twritten by thge language engineer.
+ * This class is initially empty,  and will not be overwritten if it already exists..
+ */
+export class ExpressionsInterpreter extends ExpressionsInterpreterBase {
+	constructor(m: IMainInterpreter) {
+		super();
+		main = m;
+	}
+}
