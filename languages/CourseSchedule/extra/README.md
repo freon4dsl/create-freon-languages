752ecfc31ece1eeffb794b29e0642fe0997aecd8
Jos Warmer <jos.warmer@openmodeling.nl>
2025-01-25 13:19:58 +0100

    cleanup

--- languages/CourseSchedule/extra/README.md
+++ languages/CourseSchedule/extra/README.md
@@ -7,6 +7,7 @@ To install a phase:
 - Remove all files in the `defs` folder (non-recursive!)
 - Copy the files from the phase folder you want to the `src/defs` folder.
 - Do the same for all folders in `src`.
+- - copy `icons8-person-94.png` from `src/external` to the `public` folder.
 - Run `npm run build`.
 
 And you can run the editor based on the phase.
--- languages/CourseSchedule/extra/phase5/src/external/externals.ts
+++ languages/CourseSchedule/extra/phase5/src/external/externals.ts
@@ -1,6 +1,9 @@
 import {setCustomComponents} from "@freon4dsl/core-svelte";
 import PersonIcon from "./PersonIcon.svelte";
+import PhoneButton from "./PhoneButton.svelte";
 import Schedule from "./Schedule.svelte";
+import StaffAccordion from "./StaffAccordion.svelte";
+
 /**
  * Configure the external components used, so Freon can find them.
  */
@@ -8,6 +11,8 @@ export function configureExternals() {
     setCustomComponents([
         { component: PersonIcon, knownAs: "PersonIcon" },
         { component: Schedule, knownAs: "Schedule" },
+        { component: PhoneButton, knownAs: "PhoneButton" },
+        { component: StaffAccordion, knownAs: "StaffAccordion" }
     ]);
 
 }
